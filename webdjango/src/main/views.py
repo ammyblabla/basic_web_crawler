e3f03fd8153d7d86c64b3c4d7969c257c828225e
Chayanin <chayanin2007@gmail.com>
2018-11-18 00:37:18 +0700

    add pagination

--- webdjango/src/main/views.py
+++ webdjango/src/main/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from myelasticsearch.search import searcher
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 
 def index(request):
     keyword = request.GET.get('q-samsung')
@@ -19,6 +20,9 @@ def search(request, keyword=None, page=1, obj=searcher()):
         result = obj.search(keyword, sum_op = 'url', SENTENCES_COUNT=2)
     except:
         result = None
+    paginator = Paginator(result, 2)
+    get_page = request.GET.get('page')
+    result = paginator.get_page(get_page)
     context = {'keyword': keyword, 'result' : result, 'page' : page}
     template = 'search.html'
 
